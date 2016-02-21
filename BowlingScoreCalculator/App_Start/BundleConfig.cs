860679465d50b6b72c7e1fd6daef30ebaf26fbae
Ertan Ergun <ertanergn@gmail.com>
2016-02-21 17:08:01 +0100

    add UI to project

--- BowlingScoreCalculator/App_Start/BundleConfig.cs
+++ BowlingScoreCalculator/App_Start/BundleConfig.cs
@@ -8,6 +8,8 @@ namespace BowlingScoreCalculator
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
+            #region Scripts
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/JQuery/jquery-{version}.js"));
 
@@ -17,9 +19,20 @@ namespace BowlingScoreCalculator
             bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
                         "~/Scripts/Knockout/knockout-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/script").Include(
+                        "~/Scripts/site.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            #endregion
 
+            #region Styles
+
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include(
+                        "~/Content/css/bootstrap.*"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                        "~/Content/css/site.css"));
+
+            #endregion
         }
     }
 }
--- BowlingScoreCalculator/Scripts/site.js
+++ BowlingScoreCalculator/Scripts/site.js
@@ -1,20 +1,58 @@
 ﻿var ScoreCalculatorModel = function () {
     var self = this;
+    self.firstFrame = ko.observable(0);
+    self.secondFrame = ko.observable(0);
+    self.bonusFrame = ko.observable(0);
+    self.frames = ko.observableArray([]);
+    self.isBonusFrameActive = ko.observable(false);
+    self.isSecondFrameActive = ko.observable(true);
+    self.score = ko.observable(0);
+    self.framesPlayed = ko.observable(0);
+    self.currentFrame = ko.observable(1);
 
+    self.firstFrame.subscribe(function (value) {
 
-    self.sendBidRequest = function () {
+        if (self.currentFrame() == 10 && value == 10)
+            self.isBonusFrameActive(true);
+        else {
+            self.bonusFrame(0);
+            self.isBonusFrameActive(true);
+        }
 
+        if (self.currentFrame() != 10 && value == 10) {
+            self.secondFrame(0);
+            self.isSecondFrameActive(false);
+        } else
+            self.isSecondFrameActive(true);
+    });
 
-        var url = '/Home/MakeBid?idSession=' + self.bidSession() + '&bid=' + self.bidingValue();
+    self.secondFrame.subscribe(function (value) {
+
+        if (self.currentFrame() == 10 && self.firstFrame() + value == 10)
+            self.isBonusFrameActive(true);
+    });
+
+    self.submitFrames = function () {
+
+        self.frames.push(
+        {
+            "First": self.firstFrame(),
+            "Second": self.secondFrame(),
+            "Third": self.bonusFrame()
+        });
         $.ajax({
             type: "POST",
-            contentType: "application/x-www-form-urlencoded",
-            url: url,
-            //data: 0, //parseFloat(self.currentBid()),
+            contentType: "application/json",
+            url: '/Home/GetScore',
+            data: JSON.stringify(self.frames()),
             success: function(data) {
                 if (data) {
-                    self.currentBidOwner(data.currentBuyer);
-                    self.currentBid(data.currentBid);
+                    self.score(data.score);
+                    self.firstFrame(0);
+                    self.secondFrame(0);
+                    self.bonusFrame(0);
+                    self.framesPlayed(self.frames().length);
+                    self.currentFrame(self.currentFrame() + 1);
                 }
             },
             error: function(xhr, err) {
